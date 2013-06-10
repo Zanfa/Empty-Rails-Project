745a76d32620d3eb92fef69560c379b12600188b
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2013-06-10 14:05:03 +0300

    Updated Capistrano script, added a noticable change in empty#test template

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,4 +12,20 @@ ssh_options[:keys] = ["~/.ssh/chef.pem"]
 
 server "ec2-54-228-148-206.eu-west-1.compute.amazonaws.com", :app, :web, :db, :primary => true
 
+desc "Zero-downtime restart of Unicorn"
+task :restart, :except => { :no_release => true } do
+  run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+end
+
+desc "Start unicorn"
+task :start, :except => { :no_release => true } do
+  run "cd #{current_path} ; bundle exec unicorn_rails -c /etc/unicorn/app.rb -D -E production"
+end
+
+desc "Stop unicorn"
+task :stop, :except => { :no_release => true } do
+  run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+end
+
+after "deploy", "restart"
 before "deploy", "deploy:setup"
